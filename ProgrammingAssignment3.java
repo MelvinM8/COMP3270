21ceb3ce3e7e59490ba9276d95b3262c08dafc4d
MelvinM8 <79693830+MelvinM8@users.noreply.github.com>
2022-02-03 18:16:41 -0600

    Update ProgrammingAssignment3.java

--- ProgrammingAssignment3.java
+++ ProgrammingAssignment3.java
@@ -1,9 +1,18 @@
-package com.company;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
 import java.util.Scanner;
-import java.util.*;
-import java.text.*;
-import java.text.DecimalFormat;
+import java.util.Arrays;
+import java.util.Random;
 
+/**
+* Authors: Melvin Moreno and Matthew Freestone
+* This is a program we made for Introduction to Algorithms with Dr. Biaz
+* It uses the merge-sort algorithm to sort a large array and outputs the time it took
+* to sort.
+*/
 public class ProgrammingAssignment3 {
     //METHODS
 
@@ -21,6 +30,7 @@ public class ProgrammingAssignment3 {
         }
         for (int j = 0; j < n2; ++j) {
             R[j] = A[q + j + 1];
+            // R[j] = A[q + j];
         }
 
         //Use 0x0fff ffff for infinity
@@ -54,39 +64,105 @@ public class ProgrammingAssignment3 {
     //Implemented MergeSort pseudocode from document.
     public static void mergeSort(int A[], int p, int r) {
         if (p < r) {
+            // This should automatically floor, as its integer division
             int q = (p + r) / 2;
+            
             mergeSort(A, p, q);
             mergeSort(A, q+1, r);
             merge(A, p, q, r);
         }
-    }    //Check to see if sorts an array of 10.
-    public static void array(int arrayTest[]) {
-        int n = arrayTest.length;
-        for (int i = 0; i < n; ++i) {
-            System.out.print(arrayTest[i] + " ");
-            System.out.println();
+    }   
+    
+    /**
+     * Generates a random array 
+     * @param n Length of the random array
+     * @return Pointer to the array 
+     */
+    public static int[] generateRandomArray(int n, int max_value){
+        int out[] = new int[n];
+
+        Random random = new Random();
+        for (int i = 0; i < out.length; ++i){
+            out[i] = random.nextInt(max_value);
         }
+        return out;
+    }
+    
+    public static void testRun(Scanner stdin){
+        System.out.print("Input how many elements are in the random array: ");
+        int n = stdin.nextInt();
+        int testArray[] = generateRandomArray(n, n);
+        
+        System.out.println("Unsorted Array: ");
+        System.out.println(Arrays.toString(testArray));
+
+        long start = System.currentTimeMillis();
+        mergeSort(testArray, 0, testArray.length-1);
+        long end = System.currentTimeMillis();
+
+        System.out.println("After Sorting the Array is: ");
+        System.out.println(Arrays.toString(testArray));
+
+
+        System.out.println("It took " + (end - start) + " ms to sort.");
+        stdin.close();
+        return;
+    }
+    public static void dataRun(Scanner stdin){
+        String FILENAME = "output.csv";
+        OutputStream outputStream;
+        try {
+            outputStream = new FileOutputStream(new File(FILENAME));
+        }
+        catch (FileNotFoundException e){
+            e.printStackTrace();
+            stdin.close();
+            return;
+        }
+
+        System.out.print("Enter the size of the largest array to sort (max 2^31): ");
+        int max_value = stdin.nextInt();
+        int largeRandom[] = generateRandomArray(max_value, Integer.MAX_VALUE);
+        String result = "n, time (ms)\n";
+        for (int n = 1000; n <= max_value; n+=1000 ){
+            int currentProblem[] = new int[n];
+            for (int i = 0; i < currentProblem.length; i++){
+                currentProblem[i] = largeRandom[i];
+            }
+
+            long start = System.currentTimeMillis();
+            ProgrammingAssignment3.mergeSort(currentProblem, 0, currentProblem.length-1);
+            long end = System.currentTimeMillis();
+            result +=  n+ ", " + (end-start)+"\n";
+        }
+        
+        try {
+            outputStream.write(result.getBytes());
+            outputStream.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+            stdin.close();
+            return;
+        }
+        stdin.close();
     }
 
     public static void main(String[] args) {
+        System.out.println("This program has two modes, select the one you want.");
+        System.out.println("\t1) Test - enter an array size, and the program will sort a random array of that size.");
+        System.out.println("\t2) Data - the program will sort very large arrays and store the time taken in a file.");
+        System.out.print("Enter 1 or 2: ");
+
         Scanner stdin = new Scanner(System.in);
-        int i, n;
-        long start = System.currentTimeMillis();
-        System.out.println("Input how many elements are in the array: ");
-        n = stdin.nextInt();
-        int arrayTest[] = new int[n];
-        System.out.println("Enter the elements of the array: " );
-        for (i = 0; i < n; i++) {
-            arrayTest[i] = stdin.nextInt();
+        int result = stdin.nextInt();
+        if (result == 1){
+            testRun(stdin);
         }
-        System.out.println("Unsorted Array: ");
-        array(arrayTest);
-        ProgrammingAssignment3 obj = new ProgrammingAssignment3();
-        obj.mergeSort(arrayTest, 0, n-1);
-        long end = System.currentTimeMillis();
-        System.out.println("After Sorting the Array is: ");;
-        array(arrayTest);
-        NumberFormat formatter = new DecimalFormat("#0.00000");
-        System.out.print("It took " + formatter.format((end - start) / 1000d) + " seconds to sort.");
+        else if (result == 2){
+            dataRun(stdin);
+        }
+        else {
+            System.out.println("Bad input, exiting.");
+        }
     }
 }
